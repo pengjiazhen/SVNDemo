ea2d694d1ed9cc0ccf52997b3699e4757047c34e
smile666666 <1752234827@qq.com>
2016-03-31 15:11:31 +0800

    Initial commit

---
+++ app/src/main/java/com/edu/svndemo/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.edu.svndemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView mainText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
